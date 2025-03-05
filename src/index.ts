d8630f8eb54143ebbc8cff3f098c9418a1b67cc4
Dmitry Pustovalov <pustovalov.dmitry.v@yandex.ru>
2025-03-05 16:25:06 +0300

    feat(app): Add albumUrlParser; getPrice: number and it red if = 0;

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,9 @@ import ExcelJS from 'exceljs'
 import * as dotenv from 'dotenv'
 import fs from 'fs'
 
-// https://vk.com/album-225190306_307435564
-// https://vk.com/album-225190306_307783090
-// https://vk.com/album-225190306_307826349
-// https://vk.com/album-225190306_307858232
-
-const OWNER_ID = -225190306
-const ALBUM_ID = 307858232
+// const OWNER_ID = -225190306
+// const ALBUM_ID = 307858232
+const ALBUM_URL = 'https://vk.com/album-225190306_307826349'
 const IMAGES_FOLDER = '.temp/images/'
 
 dotenv.config()
@@ -24,6 +20,13 @@ const instance = axios.create({
     v: '5.199',
   },
 })
+
+const albumUrlParser = (url: string) => {
+  const regexp = /(album)(-[\d]+)_([\d]+)/
+  const result = regexp.exec(url)
+  return {OWNER_ID: result?.[2], ALBUM_ID: result?.[3]}
+}
+const {OWNER_ID, ALBUM_ID} = albumUrlParser(ALBUM_URL)
 
 const getAllAlbumComments = async (axios: AxiosInstance) => {
   return await axios
@@ -106,18 +109,17 @@ const getAlbumPhotosOrder = async (axios: AxiosInstance) => {
 }
 
 const calculateLines = (text: string, charsPerLine: number): number => {
-  return text
-    .split('\n')
-    .reduce((acc: number, currentLine: string) => {
-      acc += Math.ceil(currentLine.length / charsPerLine)
-      return acc
-    }, 0)
+  return text.split('\n').reduce((acc: number, currentLine: string) => {
+    acc += Math.ceil((currentLine.length + 1) / charsPerLine)
+    return acc
+  }, 0)
 }
 
 const getPrice = (text: string) => {
   const regexp = /(?:Стоимость|Цена)\s*[:—-\s]*\s*([^₽\n]+)/i
   const result = regexp.exec(text)
-  return result?.[1] ?? 0
+  const price = Number(result?.[1])
+  return Number.isNaN(price) ? 0 : price
 }
 
 const exportToExcel = async (
@@ -162,12 +164,15 @@ const exportToExcel = async (
     },
     {width: 11},
     {width: 11},
+    {header: '№', key: 'photo_num', width: 5},
   ]
 
   const albumPhotosOrder = await getAlbumPhotosOrder(instance)
 
+  let price = 0
   comments.forEach((comment, index, comments) => {
     if (index === 0 || comment.pid !== comments[index - 1]?.pid) {
+      // Определяем, что это первая строка нового/следующего товара
       const row = worksheet.addRow({})
       const rowNumber = row.number
       const imageId = workbook.addImage({
@@ -185,22 +190,41 @@ const exportToExcel = async (
         (photo) => photo.id === comment.pid
       ).text
       descriptionCell.value = descriptionText
-      priceCell.value = getPrice(descriptionText)
-      imageRow.height = Math.max(77, calculateLines(descriptionText, 26) * 20)
+      price = getPrice(descriptionText) // получаем цену из описания и присваиваем ее значение в price
+      priceCell.value = price
+      imageRow.height = Math.max(77, calculateLines(descriptionText, 26) * 14)
     }
-
+    const commentOrder = albumPhotosOrder.indexOf(comment.pid) + 1
+    const userName = comment.from
+      ? `${comment.from.first_name} ${comment.from.last_name}`
+      : ''
     const row = worksheet.addRow({
       photo_url: {
-        text: albumPhotosOrder.indexOf(comment.pid) + 1,
+        text: commentOrder,
         hyperlink: comment.photo_url,
       },
-      user_name: comment.from
-        ? `${comment.from.first_name} ${comment.from.last_name}`
-        : 'Я',
+      user_name: userName,
       text: comment.text,
+      price: price,
+      photo_num: commentOrder,
     })
 
-    row.height = calculateLines(comment.text, 28) * 17 // Определяем высоту строки по количеству текста комментария
+    // Получаем ячейку с ценой (колонка "price")
+    const priceCell = row.getCell('price')
+
+    // Если цена 0, применяем стиль с красным фоном
+    if (price === 0) {
+      priceCell.fill = {
+        type: 'pattern',
+        pattern: 'solid',
+        fgColor: {argb: 'FFFF0000'}, // Красный цвет
+      }
+    }
+
+    row.height = Math.max(
+      calculateLines(comment.text, 28) * 14,
+      calculateLines(userName, 20) * 14
+    ) // Определяем высоту строки по количеству текста комментария/имени
 
     if (comment.attachments) {
       const commentPhotos = comment.attachments
@@ -235,6 +259,7 @@ const allComments: any[] = await getAllAlbumComments(instance) // Get all commen
 
 // sort comments
 allComments.reverse().sort((a, b) => a.pid - b.pid)
+console.log('allComments.length :', allComments.length)
 
 const photosIds = new Set<string>()
 const usersIds = new Set<number>()
